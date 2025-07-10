8f06a7798e256b767745cdaa62c1d130d704037e
Dan <157068051+danhicks853@users.noreply.github.com>
2025-07-10 19:39:59 -0400

    faction filter

---
+++ src/synastrialoot/hashFactionItems.lua
@@ -0,0 +1,8 @@
+-- Table mapping itemID to faction: 1 = Alliance, 2 = Horde
+-- Example: [12345] = 1, -- Alliance only
+--          [67890] = 2, -- Horde only
+local hashFactionItems = {
+    -- TODO: Fill with real data
+}
+
+return hashFactionItems
